e4e6c6d03a036381cb76a8b9f8368aa2d079d401
Josef <josefnatanael@gmail.com>
2021-02-13 00:27:58 +0800

    Debugging matters

--- app.py
+++ app.py
@@ -6,9 +6,7 @@ from linebot import (
     LineBotApi, WebhookHandler
 )
 from flask import Flask, request, abort
-from dotenv import load_dotenv
 import os
-load_dotenv()
 
 
 app = Flask(__name__)
@@ -37,6 +35,8 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
+    print(message)
+    print(event)
     line_bot_api.reply_message(event.reply_token, message)
 
 
