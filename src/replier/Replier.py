ba900aab8022745f4b1bd17de5f4f393a20a9a8a
Josef <josefnatanael@gmail.com>
2021-02-16 03:11:40 +0800

    License headers

--- src/replier/Replier.py
+++ src/replier/Replier.py
@@ -1,3 +1,23 @@
+# Copyright (C) 2021 Josef Natanael
+# 
+# This file is part of BoyBot LINE bot.
+# 
+# BoyBot LINE bot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# BoyBot LINE bot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with BoyBot LINE bot.  If not, see <http://www.gnu.org/licenses/>.
+# 
+# This copyright notice and this permission notice shall be included in 
+# all copies or substantial portions of the Software.
+
 import os
 import datetime
 
