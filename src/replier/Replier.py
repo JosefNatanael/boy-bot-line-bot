5be96d014c3116ad54e9fe353431b376bfe90e57
Josef <josefnatanael@gmail.com>
2021-02-17 20:37:01 +0800

    Debug

--- src/replier/Replier.py
+++ src/replier/Replier.py
@@ -269,6 +269,7 @@ class Replier:
         try:
             text_message = ""
             clean_message = self.message.lower().strip()
+            logger.notice(clean_message)
             for reply_text, keywords in constants.text_dict.items():
                 for keyword in keywords:
                     if keyword.strip() == clean_message:
@@ -278,6 +279,7 @@ class Replier:
                     continue
                 break
 
+            logger.notice(text_message)
             if text_message == "":
                 return False
             else:
